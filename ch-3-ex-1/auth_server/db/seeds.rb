059dc6420e7229205af989618496e400c7f22212
Dana Scheider <dana.scheider@gmail.com>
2022-10-11 10:08:07 +1100

    Add database seeds for apps that require particular models

--- ch-3-ex-1/auth_server/db/seeds.rb
+++ ch-3-ex-1/auth_server/db/seeds.rb
@@ -1,7 +1,22 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+module Seeds
+  module_function
+
+  def seed!
+    return if Client.find_by(client_id: 'oauth-client-1').present?
+
+    Client.create!(
+      client_id: 'oauth-client-1',
+      client_secret: 'oauth-client-1-secret',
+      scope: %w[foo bar],
+      redirect_uris: ['http://localhost:4000/callback']
+    )
+  end
+end
+
+Seeds.seed!
