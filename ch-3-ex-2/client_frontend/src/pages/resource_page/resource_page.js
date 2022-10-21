11f71a92d728691b30edece00a1e1458216c435f
Dana Scheider <dana.scheider@gmail.com>
2022-10-22 10:35:22 +1100

    Flesh out functionality to fetch protected resource from the back end

--- ch-3-ex-2/client_frontend/src/pages/resource_page/resource_page.js
+++ ch-3-ex-2/client_frontend/src/pages/resource_page/resource_page.js
@@ -26,24 +26,26 @@ const ResourcePage = () => {
     <>
       <Nav />
       <PageBody>
-        <h2 className={styles.header}>Data from protected resource:</h2>
         {error ? <ErrorContent error={error} /> :
-          <div className={styles.data}>
-            [
-            <ul className={styles.resourceList}>
-              {resource.map(element => (
-                <li className={styles.listItem} key={element.id}>
-                  &emsp;&emsp;&#123;
-                  <ul className={styles.attributes}>
-                    <li className={styles.itemText}>&emsp;&emsp;&emsp;&emsp;"name": "{element.name}",</li>
-                    <li className={styles.itemText}>&emsp;&emsp;&emsp;&emsp;"description": "{element.description}"</li>
-                  </ul>
-                  &emsp;&emsp;&#125;{element === resource[resource.length - 1] ? '' : ','}
-                </li>
-              ))}
-            </ul>
-            ]
-          </div>}
+          <>
+            <h2 className={styles.header}>Data from protected resource:</h2>
+            <div className={styles.data}>
+              [
+              <ul className={styles.resourceList}>
+                {resource.map(element => (
+                  <li className={styles.listItem} key={element.id}>
+                    &emsp;&emsp;&#123;
+                    <ul className={styles.attributes}>
+                      <li className={styles.itemText}>&emsp;&emsp;&emsp;&emsp;"name": "{element.name}",</li>
+                      <li className={styles.itemText}>&emsp;&emsp;&emsp;&emsp;"description": "{element.description}"</li>
+                    </ul>
+                    &emsp;&emsp;&#125;{element === resource[resource.length - 1] ? '' : ','}
+                  </li>
+                ))}
+              </ul>
+              ]
+            </div>
+          </>}
       </PageBody>
     </>
   )
