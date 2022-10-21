6eefcf1b06d0e1ad1815000840ec42ed5ec8d85b
Dana Scheider <dana.scheider@gmail.com>
2022-10-22 10:39:01 +1100

    Add Resource model

--- ch-3-ex-2/protected_resource/db/seeds.rb
+++ ch-3-ex-2/protected_resource/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+module Seeds
+  module_function
+
+  def seed!
+    Resource.create!(
+      name: 'Protected Resource',
+      description: 'This data has been protected by OAuth 2.0'
+    )
+  end
+end
