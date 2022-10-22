4815a2a3d5a8b9b420c4b2695ffcff9519c1dc87
Dana Scheider <dana.scheider@gmail.com>
2022-10-22 12:04:10 +1100

    Make AccessToken model readonly for protected resource

--- ch-3-ex-1/protected_resource/app/models/shared_model.rb
+++ ch-3-ex-1/protected_resource/app/models/shared_model.rb
@@ -5,4 +5,22 @@ class SharedModel < ActiveRecord::Base
 
   connects_to database: { reading: :shared }
   establish_connection :shared
+
+  def readonly?
+    true
+  end
+
+  def destroy
+    raise ActiveRecord::ReadOnlyRecord.new("Protected resource cannot manage #{pluralized_model_name}.")
+  end
+
+  def delete
+    raise ActiveRecord::ReadOnlyRecord.new("Protected resource cannot manage #{pluralized_model_name}.")
+  end
+
+  private
+
+  def pluralized_model_name
+    self.class.to_s.underscore.humanize.downcase.pluralize
+  end
 end
