55788b76c6910ad7c272ae321a3aacb33f1412d6
Dana Scheider <dana.scheider@gmail.com>
2022-10-22 17:12:40 +1100

    Correct client secret, which didn't match the client secret in the auth server

--- ch-3-ex-1/client/config/configatron/defaults.rb
+++ ch-3-ex-1/client/config/configatron/defaults.rb
@@ -1,7 +1,7 @@
 configatron.oauth do |oauth|
   oauth.client do |client|
     client.client_id = 'oauth-client-1'
-    client.client_secret = 'oauth-client-secret-1'
+    client.client_secret = 'oauth-client-1-secret'
     client.redirect_uris = ['http://localhost:4000/callback']
   end
 
