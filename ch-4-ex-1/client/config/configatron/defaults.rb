ef203272044ae0bcfe68cba2ebde06b55ab395e7
Dana Scheider <dana.scheider@gmail.com>
2022-10-23 19:51:43 +1100

    Update defaults to match book's example

--- ch-4-ex-1/client/config/configatron/defaults.rb
+++ ch-4-ex-1/client/config/configatron/defaults.rb
@@ -2,13 +2,16 @@ configatron.oauth do |oauth|
   oauth.client do |client|
     client.client_id = 'oauth-client-1'
     client.client_secret = 'oauth-client-secret-1'
-    client.scope = 'foo'
+    client.scope = ''
     client.redirect_uris = ['http://localhost:4000/callback']
   end
 
   oauth.auth_server do |auth_server|
     auth_server.authorization_endpoint = 'http://localhost:4003/authorize'
     auth_server.token_endpoint = 'http://localhost:4003/token'
+    auth_server.revocation_endpoint = 'http://localhost:4003/revoke'
+    auth_server.registration_endpoint = 'http://localhost:4003/register'
+    auth_server.user_info_endpoint = 'http://localhost:4003/user_info'
   end
 
   oauth.resource do |resource|
