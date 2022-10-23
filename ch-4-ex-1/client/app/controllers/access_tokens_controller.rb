1cc1d90a5f35c6b646da51d099fa7609ed74e818
Dana Scheider <dana.scheider@gmail.com>
2022-10-23 20:21:08 +1100

    Return status 204 if there are no access tokens saved

--- ch-4-ex-1/client/app/controllers/access_tokens_controller.rb
+++ ch-4-ex-1/client/app/controllers/access_tokens_controller.rb
@@ -2,6 +2,11 @@
 
 class AccessTokensController < ApplicationController
   def token
+    if AccessToken.count == 0
+      head :no_content
+      return
+    end
+
     render json: { token: AccessToken.last }, status: :ok
   end
 end
