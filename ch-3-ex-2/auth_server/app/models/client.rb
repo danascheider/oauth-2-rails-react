38e9e30eccb4c9dc1ffb4210e86c034e0bb0366a
Dana Scheider <dana.scheider@gmail.com>
2022-11-03 08:07:54 +1100

    Add dependent option to client associations

--- ch-3-ex-2/auth_server/app/models/client.rb
+++ ch-3-ex-2/auth_server/app/models/client.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Client < ApplicationRecord
-  has_many :requests, foreign_key: 'client_id', primary_key: 'client_id'
-  has_many :access_tokens, foreign_key: 'client_id', primary_key: 'client_id'
-  has_one :refresh_token, foreign_key: 'client_id', primary_key: 'client_id'
+  has_many :requests, foreign_key: 'client_id', primary_key: 'client_id', dependent: :destroy
+  has_many :access_tokens, foreign_key: 'client_id', primary_key: 'client_id', dependent: :destroy
+  has_one :refresh_token, foreign_key: 'client_id', primary_key: 'client_id', dependent: :destroy
 
   validates :client_id, presence: true, uniqueness: true
   validates :client_secret, presence: true
