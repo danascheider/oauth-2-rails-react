609eb486641c76b94565db255cb0bc2e14ec8d96
Dana Scheider <dana.scheider@gmail.com>
2022-12-26 11:34:06 -0800

    Small refactor

--- ch-4-ex-1/client/app/controllers/access_tokens_controller.rb
+++ ch-4-ex-1/client/app/controllers/access_tokens_controller.rb
@@ -4,9 +4,8 @@ class AccessTokensController < ApplicationController
   def token
     if AccessToken.count == 0
       head :no_content
-      return
+    else
+      render json: { token: AccessToken.last }, status: :ok
     end
-
-    render json: { token: AccessToken.last }, status: :ok
   end
 end
