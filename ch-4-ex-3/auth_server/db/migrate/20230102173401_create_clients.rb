2c3652cae65f362c65319fa8dc684e7733d75fbc
Dana Scheider <dana.scheider@gmail.com>
2023-01-02 09:43:55 -0800

    Create Client model and set up database

---
+++ ch-4-ex-3/auth_server/db/migrate/20230102173401_create_clients.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :client_id, null: false
+      t.string :client_secret, null: false
+      t.string :scope, array: true, null: false, default: []
+      t.string :redirect_uris, array: true, null: false
+
+      t.timestamps
+    end
+  end
+end
--- ch-4-ex-3/auth_server/db/seeds.rb
+++ ch-4-ex-3/auth_server/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+module Seeds
+  module_function
+
+  def seed!
+    seed_client!
+  end
+
+  def seed_client!
+    Client.create!(
+      client_id: 'oauth-client-1',
+      client_secret: 'oauth-client-secret-1',
+      scope: %w[fruit veggies meats],
+      redirect_uris: ['http://localhost:4000/callback']
+    )
+  rescue ActiveRecord::RecordInvalid
+    Rails.logger.info 'Client already exists.'
+  end
+end
+
+Seeds.seed!
