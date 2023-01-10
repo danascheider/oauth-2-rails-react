d35bcef60a32972bff9be174bdde9d172d25020f
Dana Scheider <dana.scheider@gmail.com>
2023-01-11 09:41:22 +1100

    Finish integration specs for #approve endpoint

--- ch-4-ex-3/auth_server/spec/requests/approve_spec.rb
+++ ch-4-ex-3/auth_server/spec/requests/approve_spec.rb
@@ -21,34 +21,136 @@ RSpec.describe 'AuthorizationsController#approve', type: :request do
     let(:response_type) { nil }
 
     context 'when authorization is approved' do
-      context 'when the response_type is "code"' do
-        context 'when all scopes are valid'
+      context 'when disallowed scopes are present' do
+        let(:body_params) do
+          {
+            reqid: 'foobar',
+            user: 'doesntmatter',
+            scope_veggies: '1',
+            scope_fruit: '0',
+            scope_meat: '1',
+            approve: true
+          }
+        end
 
-        context 'when disallowed scopes are present'
+        it 'redirects with an error' do
+          approve
+          expect(response).to redirect_to "#{req.redirect_uri}?error=invalid_scope"
+        end
+      end
+
+      context 'when the response_type is "code"' do
+        let(:response_type) { 'code' }
+
+        context 'when a user has been selected' do
+          let!(:user) { create(:user) }
+
+          let(:body_params) do
+            {
+              reqid: 'foobar',
+              user: user.sub,
+              scope_veggies: '1',
+              scope_fruit: '0',
+              approve: true
+            }
+          end
+
+          before do
+            allow(SecureRandom).to receive(:hex).with(8).and_return('foobar')
+          end
+
+          context 'when a state value is present' do
+            let(:state) { 'raboof' }
+
+            it 'redirects successfully with the state value' do
+              approve
+              expect(response)
+                .to redirect_to("#{req.redirect_uri}?code=foobar&state=raboof")
+            end
+          end
+
+          context 'when no state value is present' do
+            it 'redirects successfully with no state param' do
+              approve
+              expect(response)
+                .to redirect_to("#{req.redirect_uri}?code=foobar")
+            end
+          end
+        end
+
+        context 'when no user has been selected' do
+          let(:body_params) do
+            {
+              reqid: 'foobar',
+              user: 'doesntexist',
+              scope_veggies: '1',
+              scope_fruit: '0',
+              approve: true
+            }
+          end
+
+          it 'returns a 500 error' do
+            approve
+            expect(response.status).to eq 500
+          end
+        end
       end
 
       context 'when the response_type is "token"' do
-        context 'when all scopes are valid' do
-          context 'when a user has been selected' do
-            context 'when the request object has a "state" value'
+        let(:response_type) { 'token' }
 
-            context "when the request object doesn't have a 'state' value"
+        context 'when a user has been selected' do
+          let!(:user) { create(:user) }
+
+          let(:body_params) do
+            {
+              reqid: 'foobar',
+              user: user.sub,
+              scope_veggies: '1',
+              scope_fruit: '0',
+              approve: true
+            }
           end
 
-          context 'when no user has been selected'
+          context 'when the request object has a "state" value' do
+            let(:state) { 'raboof' }
+
+            it 'redirects with the state value' do
+              approve
+              expect(response)
+                .to redirect_to("https://example.com/callback?access_token=#{AccessToken.last.token}&token_type=Bearer&scope=fruit+veggies&client_id=#{client.client_id}&user=#{user.sub}&refresh_token=#{RefreshToken.last.token}&state=raboof")
+            end
+          end
+
+          context "when the request object doesn't have a 'state' value"
         end
 
-        context 'when disallowed scopes are present'
+        context 'when no user has been selected' do
+          let(:body_params) do
+            {
+              reqid: 'foobar',
+              user: 'baz',
+              scope_veggies: '1',
+              scope_fruit: '0',
+              approve: true
+            }
+          end
+
+          it 'returns a 500 error' do
+            approve
+            expect(response.status).to eq 500
+          end
+        end
       end
 
       context 'when there is a different response type' do
+        let!(:user) { create(:user) }
         let(:response_type) { 'foo' }
 
         let(:body_params) do
           {
             reqid: 'foobar',
-            client_id: client.client_id,
-            user: 'doesntmatter',
+            user: user.sub,
             scope_fruit: '1',
             scope_veggies: '1',
             approve: true
@@ -66,7 +168,6 @@ RSpec.describe 'AuthorizationsController#approve', type: :request do
       let(:body_params) do
         {
           reqid: 'foobar',
-          client_id: client.client_id,
           user: 'doesntmatter',
           scope_fruit: '1',
           scope_veggies: '1'
