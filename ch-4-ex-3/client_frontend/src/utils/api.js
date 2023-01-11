45694d8c078e839cc339bfdc9f117267c5b10417
Dana Scheider <dana.scheider@gmail.com>
2023-01-12 10:12:40 +1100

    Prevent second request to authorization server leading to error with duplicate state

--- ch-4-ex-3/client_frontend/src/utils/api.js
+++ ch-4-ex-3/client_frontend/src/utils/api.js
@@ -3,5 +3,5 @@ const baseUri = 'http://localhost:4001'
 export const getAuthorize = () => {
   const uri = `${baseUri}/authorize`
 
-  return fetch(uri)
+  return fetch(uri, { redirect: 'manual' })
 }
