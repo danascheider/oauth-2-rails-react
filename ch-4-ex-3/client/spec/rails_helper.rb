3fc6f510ac427f3d3269210ccb90c855d098227f
Dana Scheider <dana.scheider@gmail.com>
2023-01-12 13:30:50 +1100

    Add WebMock to test HTTP requests

--- ch-4-ex-3/client/spec/rails_helper.rb
+++ ch-4-ex-3/client/spec/rails_helper.rb
@@ -34,6 +34,7 @@ DatabaseCleaner[:active_record].strategy = :transaction
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include WebMock::API
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
