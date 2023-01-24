9a9a737c78342bb98afeee4eddeddbbdc9612cb4
Dana Scheider <dana.scheider@gmail.com>
2023-01-25 07:49:48 +1100

    Correct URL and add getAuthorize function to prevent errors

--- ch-4-ex-4/client_frontend/src/utils/api.js
+++ ch-4-ex-4/client_frontend/src/utils/api.js
@@ -1,7 +1,13 @@
 const baseUri = 'http://localhost:4001'
 
+export const getAuthorize = () => {
+  const uri = `${baseUri}/authorize`
+
+  return fetch(uri, { redirect: 'manual' })
+}
+
 export const getToken = () => {
-  const uri = `${baseUri}/tokens`
+  const uri = `${baseUri}/token`
 
   return fetch(uri)
 }
