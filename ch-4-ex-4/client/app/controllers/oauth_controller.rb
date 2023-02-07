6157b509e176eb951e8058747720af963b8e8fd7
Dana Scheider <dana.scheider@gmail.com>
2023-02-08 08:13:48 +1100

    Flesh out callback endpoint handler

--- ch-4-ex-4/client/app/controllers/oauth_controller.rb
+++ ch-4-ex-4/client/app/controllers/oauth_controller.rb
@@ -21,5 +21,73 @@ class OauthController < ApplicationController
   end
 
   def callback
+    if query_params[:error].present?
+      render json: { error: query_params[:error] }, status: :forbidden
+      return
+    end
+
+    if authorization_request.nil?
+      Rails.logger.error "State value '#{query_params[:state]}' did not match an existing authorization request"
+      render json: { error: 'State value did not match' }, status: :forbidden
+      return
+    end
+
+    Rails.logger.info "State value '#{query_params[:state]}' matches"
+
+    form_data = URI.encode_www_form({
+                                      grant_type: 'authorization_code',
+                                      code: query_params[:code],
+                                      redirect_uri: default_redirect_uri
+                                    })
+
+    headers = {
+      'Content-Type' => 'application/x-www-form-urlencoded',
+      'Authorization' => "Basic #{client_credentials}"
+    }
+
+    Rails.logger.info "Requesting access token for code '#{query_params[:code]}'"
+
+    token_response = Faraday.post(configatron.oauth.auth_server.token_endpoint, form_data, headers)
+
+    if token_response.success?
+      body = JSON.parse(token_response.body, symbolize_names: true)
+
+      access_token, refresh_token, scope, user, token_type = body.values_at(:access_token, :refresh_token, :scope, :user, :token_type)
+
+      Rails.logger.info "Got access token: '#{access_token}'"
+      Rails.logger.info "Got refresh token: '#{refresh_token}'"
+      Rails.logger.info "Got scope: '#{scope}'"
+
+      AccessToken.create!(
+        access_token:,
+        refresh_token:,
+        user:,
+        scope: scope.split(' '),
+        token_type:
+      )
+
+      render json: { access_token:, refresh_token:, scope: }, status: :ok
+    else
+      Rails.logger.error JSON.parse(token_response.body, symbolize_names: true)[:error]
+      error = "Unable to fetch access token, error (#{token_response.status})"
+      render json: { error: }, status: token_response.status
+    end
+  end
+
+  private
+
+  def authorization_request
+    @authorization_request ||= AuthorizationRequest.find_by(state: query_params[:state])
+  end
+
+  def default_redirect_uri
+    configatron.oauth.client.default_redirect_uri
+  end
+
+  def client_credentials
+    client_id = CGI.escape(configatron.oauth.client.client_id)
+    client_secret = CGI.escape(configatron.oauth.client.client_secret)
+
+    Base64.encode64("#{client_id}:#{client_secret}")
   end
 end
--- ch-4-ex-4/client/spec/requests/oauth_spec.rb
+++ ch-4-ex-4/client/spec/requests/oauth_spec.rb
@@ -43,4 +43,185 @@ RSpec.describe 'OauthController', type: :request do
       expect { authorize }.to change(AuthorizationRequest, :count).from(0).to(1)
     end
   end
+
+  describe 'GET /callback' do
+    subject(:callback) { get callback_path, params: }
+
+    context 'when the params include an error' do
+      let(:params) { { error: 'unsupported_response_type' } }
+
+      it 'returns the error to the client' do
+        callback
+        expect(JSON.parse(response.body)).to eq({ 'error' => 'unsupported_response_type' })
+      end
+
+      it 'returns status 403' do
+        callback
+        expect(response).to be_forbidden
+      end
+    end
+
+    context "when the state value doesn't match an existing request" do
+      let(:params) do
+        {
+          state: 'foobar',
+          code: SecureRandom.hex(8)
+        }
+      end
+
+      before do
+        allow(Rails.logger).to receive(:error)
+      end
+
+      it 'logs the error' do
+        callback
+        expect(Rails.logger)
+          .to have_received(:error)
+                .with("State value 'foobar' did not match an existing authorization request")
+      end
+
+      it 'renders an error' do
+        callback
+        expect(JSON.parse(response.body))
+          .to eq({ 'error' => 'State value did not match' })
+      end
+
+      it 'returns status 403' do
+        callback
+        expect(response).to be_forbidden
+      end
+    end
+
+    context 'when there is an existing authorization request' do
+      let!(:request) { create(:authorization_request) }
+      let(:faraday_response) { instance_double(Faraday::Response, body:, status:, success?: success) }
+      let(:status) { 200 }
+      let(:success) { true }
+
+      let(:body) do
+        {
+          access_token: 'foo',
+          refresh_token: 'raboof',
+          token_type: 'Bearer',
+          scope: 'movies foods',
+          user: 'user-42'
+        }.to_json
+      end
+
+      let(:params) do
+        {
+          state: request.state,
+          code: 'foobar'
+        }
+      end
+
+      let(:expected_request_body) do
+        URI.encode_www_form({
+                              grant_type: 'authorization_code',
+                              code: 'foobar',
+                              redirect_uri: configatron.oauth.client.default_redirect_uri
+                            })
+      end
+
+      let(:expected_reqeust_headers) do
+        {
+          'Content-Type' => 'application/x-www-form-urlencoded',
+          'Authorization' => /^Basic \w+$/
+        }
+      end
+
+      before do
+        allow(Rails.logger).to receive(:info)
+        allow(Faraday).to receive(:post).and_return(faraday_response)
+      end
+
+      it 'logs the state' do
+        callback
+        expect(Rails.logger)
+          .to have_received(:info)
+                .with("State value '#{request.state}' matches")
+      end
+
+      it 'logs the request to the auth server' do
+        callback
+        expect(Rails.logger)
+          .to have_received(:info)
+                .with("Requesting access token for code 'foobar'")
+      end
+
+      context 'when the auth server endpoint returns a successful response' do
+        let(:expected_response_body) do
+          {
+            access_token: 'foo',
+            refresh_token: 'raboof',
+            scope: 'movies foods'
+          }
+        end
+
+        it 'logs the access token' do
+          callback
+          expect(Rails.logger).to have_received(:info).with("Got access token: 'foo'")
+        end
+
+        it 'logs the refresh token' do
+          callback
+          expect(Rails.logger).to have_received(:info).with("Got refresh token: 'raboof'")
+        end
+
+        it 'logs the scope' do
+          callback
+          expect(Rails.logger).to have_received(:info).with("Got scope: 'movies foods'")
+        end
+
+        it 'creates an AccessToken record' do
+          expect { callback }.to change(AccessToken, :count).from(0).to(1)
+        end
+
+        it 'assigns the correct attributes', :aggregate_failures do
+          callback
+          expect(AccessToken.last.access_token).to eq 'foo'
+          expect(AccessToken.last.refresh_token).to eq 'raboof'
+          expect(AccessToken.last.user).to eq 'user-42'
+          expect(AccessToken.last.scope).to eq %w[movies foods]
+          expect(AccessToken.last.token_type).to eq 'Bearer'
+        end
+
+        it 'returns the data to the front end' do
+          callback
+          expect(JSON.parse(response.body, symbolize_names: true)).to eq expected_response_body
+        end
+
+        it 'returns status 200' do
+          callback
+          expect(response).to be_successful
+        end
+      end
+
+      context 'when the auth server returns an error response' do
+        let(:body) { { error: 'oops' }.to_json }
+        let(:status) { 421 }
+        let(:success) { false }
+
+        before do
+          allow(Rails.logger).to receive(:error)
+        end
+
+        it 'logs an error' do
+          callback
+          expect(Rails.logger).to have_received(:error).with('oops')
+        end
+
+        it 'returns an error to the client' do
+          callback
+          expect(JSON.parse(response.body, symbolize_names: true))
+            .to eq({ error: 'Unable to fetch access token, error (421)' })
+        end
+
+        it 'returns the HTTP status from the auth server' do
+          callback
+          expect(response.status).to eq 421
+        end
+      end
+    end
+  end
 end
